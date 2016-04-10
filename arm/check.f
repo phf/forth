214f719b9cf37002e9435b44e55dfc1177171ac0
Peter Froehlich <peter.hans.froehlich@gmail.com>
2016-04-10 22:11:00 +0000

    The -ROT test found a HUGE bug in the FIND primitive.

--- arm/check.f
+++ arm/check.f
@@ -1,3 +1,4 @@
+DONG
 \ Test cases for the basic FORTH interpreter, without jonesforth.f loaded.
 
 \ A word that puts 42 on the stack.
@@ -21,3 +22,20 @@ STAR
 \ Check /MOD with stars.
 120 3 /MOD 2 + EMIT
 47 100 /MOD DROP 5 - EMIT
+
+\ Test 1+ 1- 4+ 4- hacks.
+41 1+ EMIT
+43 1- EMIT
+38 4+ EMIT
+46 4- EMIT
+
+\ Test DROP SWAP OVER DUP hacks.
+42 43 DROP EMIT
+42 43 SWAP EMIT 1- EMIT
+21 DUP + EMIT
+1 42 3 OVER EMIT
+
+\ Test ROT and -ROT hacks.
+41 42 43 1- EMIT EMIT 1+ EMIT
+41 42 43 ROT 1+ EMIT 1- EMIT EMIT
+41 42 43 -ROT EMIT 1+ EMIT 1- EMIT
